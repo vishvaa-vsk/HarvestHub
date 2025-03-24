0cea1d861592527a0162b21e14b3a86c7801f986
vishvaa-vsk <kvishvaa6@gmail.com>
2025-03-24 22:54:54 +0530

    feat: Update Firebase options to use environment variables and add .env.example file

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -49,21 +50,20 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCq86wYEFXcwgzkvOoyrDA0zm3jtEHWTl4',
-    appId: '1:47719572182:android:86eb3f80ba30ea03d6df77',
-    messagingSenderId: '47719572182',
-    projectId: 'harvest-hub-c9ee5',
-    storageBucket: 'harvest-hub-c9ee5.firebasestorage.app',
+  static FirebaseOptions get android => FirebaseOptions(
+    apiKey: dotenv.env['ANDROID_API_KEY'] ?? '',
+    appId: dotenv.env['ANDROID_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['ANDROID_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['ANDROID_PROJECT_ID'] ?? '',
+    storageBucket: dotenv.env['ANDROID_STORAGE_BUCKET'] ?? '',
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCd31VAGr3sRRNZsRYrCRQ5DpXgzzCFmuI',
-    appId: '1:47719572182:ios:a483e4a441e20f59d6df77',
-    messagingSenderId: '47719572182',
-    projectId: 'harvest-hub-c9ee5',
-    storageBucket: 'harvest-hub-c9ee5.firebasestorage.app',
-    iosBundleId: 'com.example.harvesthub',
+  static FirebaseOptions get ios => FirebaseOptions(
+    apiKey: dotenv.env['IOS_API_KEY'] ?? '',
+    appId: dotenv.env['IOS_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['IOS_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['IOS_PROJECT_ID'] ?? '',
+    storageBucket: dotenv.env['IOS_STORAGE_BUCKET'] ?? '',
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID'] ?? '',
   );
-
 }
